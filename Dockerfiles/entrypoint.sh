c137eb69c2c06a23fe377c9172c3beccd5f385d5
Chyaboiii <joao.cm.albuquerque@gmail.com>
2017-07-06 12:38:23 +0100

    fix

--- Dockerfiles/entrypoint.sh
+++ Dockerfiles/entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 sed -ie "s#{antidote_nodes, .*}.#{antidote_nodes, ['${ANTIDOTE_NODE}']}.#g" $CONFIG
 sed -ie "s#{bench_node, .*}.#{bench_node, ['${BENCH_NODE}', longnames]}.#g" $CONFIG
+sed -ie "s#{bigdata, .*}.#{bigdata, '/bigdata'}.#g" $CONFIG
+sed -ie "s#{operations, .*}.#{operations, [{add, ${ADD_N}}, {add_set, ${ADD_SET_N}}]}.#g" $CONFIG
 
 touch setup_ok
 
--- Dockerfiles/run.sh
+++ Dockerfiles/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 
+epmd && sleep 100 &
 /opt/basho_bench -N $BENCH_NODE -C $COOKIE /opt/bigdata.config & wait ${!}
