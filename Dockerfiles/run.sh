161d37cb19b9837a2102850047d3c6d34cd2dacc
Chyaboiii <joao.cm.albuquerque@gmail.com>
2017-07-10 11:30:38 +0100

    .

--- Dockerfiles/run.sh
+++ Dockerfiles/run.sh
@@ -2,4 +2,5 @@
 set -e
 
 epmd && sleep 100 &
-/opt/basho_bench -N $BENCH_NODE -C $COOKIE /opt/bigdata.config && cp -r /tests/current /data wait ${!}
+/opt/basho_bench -N $BENCH_NODE -C $COOKIE /opt/bigdata.config & wait ${!} &
+cp -r /tests/current /data & wait ${!}
