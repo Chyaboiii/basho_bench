77f8f176b0557a12e5f883e7c3d03a8586de3f3f
Chyaboiii <joao.cm.albuquerque@gmail.com>
2017-07-10 10:23:21 +0100

    .

--- Dockerfiles/entrypoint.sh
+++ Dockerfiles/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 sed -ie "s#{antidote_nodes, .*}.#{antidote_nodes, ['${ANTIDOTE_NODE}']}.#g" $CONFIG
 sed -ie "s#{bench_node, .*}.#{bench_node, ['${BENCH_NODE}', longnames]}.#g" $CONFIG
-sed -ie "s#{bigdata, .*}.#{bigdata, '/bigdata'}.#g" $CONFIG
+sed -ie "s#{bigdata, .*}.#{bigdata, '/big_data'}.#g" $CONFIG
 sed -ie "s#{operations, .*}.#{operations, [{add, ${ADD_N}}, {add_set, ${ADD_SET_N}}]}.#g" $CONFIG
 
 touch setup_ok
